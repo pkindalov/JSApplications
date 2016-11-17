09f2957e81e2cf4bb2a943006efadfa9b4686b9b
pkindalov <panayotkindalov@gmail.com>
2016-11-17 10:01:16 +0200

    Added first exercise from jQuery and Ajax lesson

---
+++ jQueryAndAjax/First exercise/XMLHttpRequest.js
@@ -0,0 +1,17 @@
+/**
+ * Created by r3v3nan7 on 15.11.16.
+ */
+
+function loadRepos() {
+    let req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+      if(this.readyState == 4 && this.status == 200){
+          document.getElementById("res").textContent = this.responseText;
+      }
+    };
+
+    req.open("GET",
+    "https://api.github.com/users/testnakov/repos", true);
+
+    req.send();
+}
